951ad1c87301c89ee1a480e361cac27eb1151272
Mohit Jha <jhamohitmj8@gmail.com>
2026-08-04 11:00:14 +0530

    Interpret logistic regression coefficients and feature importance

--- ml/train_logistic_regression.py
+++ ml/train_logistic_regression.py
@@ -14,6 +14,7 @@ Responsibilities:
 
 from sklearn.linear_model import LogisticRegression
 from preprocessing import preprocess_data
+import pandas as pd
 
 def train_model():
     """
@@ -65,6 +66,50 @@ def main():
     print(model.intercept_)
     print(model.coef_)
     print(model.n_iter_)
+
+    # Model Interpretation
+    feature_names = preprocessor.get_feature_names_out()
+    
+    print(feature_names)
+    print(len(feature_names))
+    print(model.coef_.shape)
+    print(len(model.coef_[0]))
+
+    coefficient_df = pd.DataFrame(
+        {
+            "Feature": feature_names,
+            "Coefficient": model.coef_[0]
+        }
+    )
+    print(coefficient_df)
+
+    #feature with highest positive coefficient
+    coefficient_df_positive = coefficient_df.sort_values(
+        by="Coefficient", 
+        ascending=False
+    )
+    print("\nTop Positive Coefficients")
+    print(coefficient_df_positive.head(10))
+
+    #feature with highest negative coefficient
+    coefficient_df_negative = coefficient_df.sort_values(
+        by="Coefficient", 
+        ascending=True
+    )
+    print("\nTop Negative Coefficients")
+    print(coefficient_df_negative.head(10))
+
+    # Magnitude of coefficients
+    coefficient_df["Absolute Coefficient"] = (
+        coefficient_df["Coefficient"].abs()
+    )
+
+    coefficient_df_absolute = coefficient_df.sort_values(
+        by="Absolute Coefficient",
+        ascending=False
+    )
+    print("\nTop Absolute Coefficients")
+    print(coefficient_df_absolute.head(10).round(3))
 
 if __name__ == "__main__":
     main()
